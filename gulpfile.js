f26efe06414ac911ed941a8efcbb4b369a24bf8e
Brandon Baris <brandonbaris@gmail.com>
2016-09-17 01:21:38 -1000

    adds gulp-sass config

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./compartments/assets/styles'))
+});
+
+//Watch task
+gulp.task('default',function() {
+    gulp.watch('sass/**/*.scss',['styles']);
+});
