294ef2041fd746165740ef22d975b7f3a5c37fda
Brandon Baris <brandonbaris@gmail.com>
2016-09-17 15:07:34 -1000

    reconfigures gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,30 @@
-'use strict';
- 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var $ = require('gulp-load-plugins')();
+var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./compartments/assets/styles'))
+var sassPaths = [
+  'bower_components/foundation-sites/scss',
+  'bower_components/motion-ui/src'
+];
+
+// keeps gulp from crashing for scss errors
+gulp.task('sass', function () {
+
+  return gulp.src('./scss/*.scss')
+    .pipe($.sass({
+      includePaths: sassPaths
+    })
+      .on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('./compartments/assets/styles'));
 });
 
-//Watch task
-gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
+gulp.task('watch', function () {
+  gulp.watch('./scss/**/*.scss', ['sass']);
 });
+
+gulp.task('default', ['watch', 'sass']);
