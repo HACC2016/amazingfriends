11d368f58e968582f65521f64729f1f54a28ecfc
Brandon Baris <brandonbaris@gmail.com>
2016-09-21 17:31:55 -1000

    guts out old todo, adds angularfire todo, different layout (profile.html) for elders view, adds momentjs

---
+++ compartments/assets/scripts/ngfire/fireconfig.js
@@ -0,0 +1,9 @@
+// Initialize Firebase. Needs to follow the firebase main script.
+var config = {
+  apiKey: "AIzaSyAKEUBhudrymur1W2ML1UVP8pTI0sZiVpc",
+  authDomain: "amazing-friends-1473134180304.firebaseapp.com",
+  databaseURL: "https://amazing-friends-1473134180304.firebaseio.com",
+  storageBucket: "amazing-friends-1473134180304.appspot.com",
+  messagingSenderId: "30481473034"
+};
+firebase.initializeApp(config);
